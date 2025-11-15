226722cce838f311c84c8e3e3ba5553e326b8865
HarshRaolji003 <raoljiharsh@gmail.com>
2025-11-15 13:11:11 +0530

    feat: integrate @nestjs/config for environment variable management and update start:dev script

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
   imports: [
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+    }),
     TypeOrmModule.forRootAsync({
-      imports: [],
-      inject: [],
-      useFactory: () => ({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         entities: [],
         synchronize: true,
-        port: 5432,
-        username: 'postgres',
-        password: 'root',
-        host: 'localhost',
-        database: 'nestjs_masterclass',
+        port: +configService.get('DATABASE_PORT'), // The '+' converts string to number
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        host: configService.get('DATABASE_HOST'),
+        database: configService.get('DATABASE_NAME'),
       }),
     }),
   ],
